48ab5405e604f6548f7f544f19633059e1009366
Mustafa Serhat Aydın <aydinmustafaserhat@gmail.com>
2025-12-11 12:30:12 +0300

    small edit

--- data/dataloaders.py
+++ data/dataloaders.py
@@ -10,23 +10,12 @@ def get_ecoli_loaders(batch_size=32):
     ecoli_df = df[df["Dataset"] == "E.Coli proteins"]
 
     train_df = ecoli_df[ecoli_df["Split"] == "train"]
-    val_df   = ecoli_df[ecoli_df["Split"] == "val"]
-    test_df  = ecoli_df[ecoli_df["Split"] == "test"]
+    val_df = ecoli_df[ecoli_df["Split"] == "val"]
+    test_df = ecoli_df[ecoli_df["Split"] == "test"]
 
-    train_dataset = RNADataset(
-        sequences=train_df["Sequence"].tolist(),
-        labels=train_df["Value"].tolist()
-    )
-
-    val_dataset = RNADataset(
-        sequences=val_df["Sequence"].tolist(),
-        labels=val_df["Value"].tolist()
-    )
-
-    test_dataset = RNADataset(
-        sequences=test_df["Sequence"].tolist(),
-        labels=test_df["Value"].tolist()
-    )
+    train_dataset = RNADataset(sequences=train_df["Sequence"].tolist(), labels=train_df["Value"].tolist())
+    val_dataset = RNADataset(sequences=val_df["Sequence"].tolist(), labels=val_df["Value"].tolist())
+    test_dataset = RNADataset(sequences=test_df["Sequence"].tolist(), labels=test_df["Value"].tolist())
 
     train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
     val_loader   = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
