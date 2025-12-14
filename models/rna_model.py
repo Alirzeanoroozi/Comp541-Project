6b240812af8dbbe85402e5943f2c2077a150aa98
Alirzeanoroozi <noroozi.alirezaa@gmail.com>
2025-12-14 16:30:39 +0300

    update rna_model

--- models/rna_model.py
+++ models/rna_model.py
@@ -10,12 +10,9 @@ class RNAFMEmbedder(nn.Module):
 
         # Load RNA-FM model and alphabet from the fm package
         self.model, self.alphabet = fm.pretrained.rna_fm_t12()
+        self.model.eval()
         self.model.to(self.device)
         self.batch_converter = self.alphabet.get_batch_converter()
-
-        # Freeze weights
-        for p in self.model.parameters():
-            p.requires_grad = False
 
     def forward(self, seq):
         data = [("RNA", seq[:self.max_len])]
@@ -25,13 +22,5 @@ class RNAFMEmbedder(nn.Module):
             results = self.model(batch_tokens.to(self.device), repr_layers=[12])
 
         emb = results["representations"][12].squeeze(0)  # (L, D)
-
-        # If length < max_len, pad to max_len
-        L, D = emb.shape
-        if L < self.max_len:
-            pad = torch.zeros(self.max_len - L, D, device=emb.device, dtype=emb.dtype)
-            emb = torch.cat([emb, pad], dim=0)
-        elif L > self.max_len:
-            emb = emb[:self.max_len, :]
 
         return emb  # (max_len, embedding_dim)
